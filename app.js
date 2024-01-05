d910bc5c92f58996e51961df4c1da2d3b0eeb3ba
Charles Louis <charleslouis1234@gmail.com>
2024-01-05 16:19:05 +0530

    level 3 hashing made on by using the package md5 but its not working so just done for reference

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const md5 = require("md5");
 const port = 3000;
-const encrypt = require("mongoose-encryption")
 
 const app = express();
 
@@ -21,9 +22,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]})
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
@@ -40,7 +38,7 @@ app.post("/register", async function (req, res) {
   try {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password,
+      password: md5(req.body.password),
     });
     await newUser.save();
     res.render("secrets");
@@ -51,7 +49,7 @@ app.post("/register", async function (req, res) {
 });
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username })
     .then((foundUser) => {
